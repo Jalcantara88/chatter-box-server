2f8cae40e7a5b35a1b9eebb99ff00224bda0cc4d
Julio Alcantara <jalcantara88@gmail.com>
2021-05-17 16:44:27 -0700

    installed cors and linked it to server set up

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const express = require('express');
 const socketIO = require('socket.io');
+var cors = require('cors');
 
 const server = express()
+  .use(cors())
   //.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
